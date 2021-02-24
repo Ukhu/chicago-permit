8d6195d1bec242840bd927201c0c5c868cb773c6
Osaukhumwen Iyamuosa <osaukhu.bi@gmail.com>
2021-02-24 22:42:51 +0100

    fix: correct base url filters

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://data.cityofchicago.org/resource/ydr8-5enu.json';
+export const BASE_URL = 'https://data.cityofchicago.org/resource/ydr8-5enu.json?$limit=10&$offset=0&$order=issue_date%20DESC';
 
 export const API_TOKEN = process.env.REACT_APP_API_TOKEN;
